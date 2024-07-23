57bdf4cee501c2d27d30479278c7376c311f2e9a
Tharun2331 <tharunsd23@gmail.com>
2024-07-22 23:16:57 -0400

    Initial commit

---
+++ app/src/main/java/com/example/fragments/SecondFragment.kt
@@ -0,0 +1,17 @@
+package com.example.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [SecondFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class SecondFragment : Fragment(R.layout.fragment_second) {
+    // TODO: Rename and change types of parameters
+
+}
